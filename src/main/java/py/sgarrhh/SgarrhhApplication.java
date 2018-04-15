3a68dffca4a9427da514fde0268a7aaef032a1ad
Denis Amarilla <denissaul87@gmail.com>
2018-04-15 17:22:08 -0400

    modificacion 15-04

--- src/main/java/py/sgarrhh/SgarrhhApplication.java
+++ src/main/java/py/sgarrhh/SgarrhhApplication.java
@@ -2,8 +2,14 @@ package py.sgarrhh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import py.sgarrhh.controller.FuncionController;
 
 @SpringBootApplication
+//@ComponentScan(basePackages= {"py.sgarrhh.controller"})
+//@ComponentScan(basePackageClasses=FuncionController.class)
+
 public class SgarrhhApplication {
 
 	public static void main(String[] args) {
