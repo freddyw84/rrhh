4767ebebdbbe710a6756a8b9e7d361214ccb5d3a
Carlos Vargas <freddyw84@gmail.com>
2018-04-15 22:46:16 -0400

    nueva modificacion

--- src/main/java/py/sgarrhh/controller/FuncionController.java
+++ src/main/java/py/sgarrhh/controller/FuncionController.java
@@ -1,13 +1,42 @@
 package py.sgarrhh.controller;
 
+import javax.management.AttributeValueExp;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import py.sgarrhh.entity.Funcion;
+import py.sgarrhh.repository.FuncionRepository;
 
 @Controller
 public class FuncionController {
-	@RequestMapping("/registrarFuncion")
+	
+	@Autowired
+	private FuncionRepository er;
+	
+	@RequestMapping(value="/registrarFuncion", method=RequestMethod.GET)
 	public String form() {
 		return "funcion/formFuncion";
 	}
 
+	@RequestMapping(value="/registrarFuncion", method=RequestMethod.POST)
+	public String form(Funcion funcion) {
+	
+		er.save(funcion);
+		
+		return "redirect:/registrarFuncion";
+	}
+
+	@RequestMapping("/funciones")
+	public ModelAndView listaFunciones() {
+
+		ModelAndView mv= new ModelAndView("index");
+		Iterable <Funcion> funciones= er.findAll();
+		mv.addObject("funciones",funciones);
+		return mv;
+	}
+	
 }
