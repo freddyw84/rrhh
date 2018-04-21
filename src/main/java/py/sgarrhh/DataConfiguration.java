2a5f5777da4a86168e7419e0aa35edca62fd5295
Carlos Vargas <freddyw84@gmail.com>
2018-04-21 11:03:49 -0400

    new

--- src/main/java/py/sgarrhh/DataConfiguration.java
+++ src/main/java/py/sgarrhh/DataConfiguration.java
@@ -1,16 +1,20 @@
 package py.sgarrhh;
 
+
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
+@Profile("dev")
 public class DataConfiguration {
+
 	@Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -19,8 +23,21 @@ public class DataConfiguration {
         dataSource.setUsername("root");
         dataSource.setPassword("");
         return dataSource;
-  }
-  @Bean
+    }
+	
+//	@Bean
+//
+//	public DataSource dataSource() {
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("org.postgresql.Driver");
+//		dataSource.setUrl("jdbc:postgresql://localhost:5432/rrhh_db");
+//		dataSource.setUsername("postgres");
+//		dataSource.setPassword("admin");
+//		return dataSource;
+//	}
+//	
+	
+	@Bean
 	public JpaVendorAdapter jpaVendorAdapter(){
 		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
 		adapter.setDatabase(Database.MYSQL);
@@ -29,7 +46,6 @@ public class DataConfiguration {
 		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
 		adapter.setPrepareConnection(true);
 		return adapter;
-	  
-  }
-  
+	
+	}
 }
