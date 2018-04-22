78e1a0b6d2f3c568c31f5486fad21e2a3db35cdb
denis <denis@denis-Aspire-F5-571T>
2018-04-21 22:41:47 -0400

    cambio

--- src/main/java/py/sgarrhh/DataConfiguration.java
+++ src/main/java/py/sgarrhh/DataConfiguration.java
@@ -17,7 +17,7 @@ public class DataConfiguration {
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/rrhh_db");
         dataSource.setUsername("root");
-        dataSource.setPassword("");
+        dataSource.setPassword("admin");
         return dataSource;
   }
   @Bean
