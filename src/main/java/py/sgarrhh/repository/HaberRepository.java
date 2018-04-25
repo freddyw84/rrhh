a93192c8dd7690fbeb5798fc96b061b7497cd1c5
denis <denis@denis-Aspire-F5-571T>
2018-04-24 23:10:02 -0400

    se agregó haber repository y rubro repository

---
+++ src/main/java/py/sgarrhh/repository/HaberRepository.java
@@ -0,0 +1,12 @@
+package py.sgarrhh.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import py.sgarrhh.models.Haber;
+
+
+
+
+public interface HaberRepository extends CrudRepository<Haber, String>{
+	
+}
---
+++ src/main/java/py/sgarrhh/repository/RubroRepository.java
@@ -0,0 +1,10 @@
+package py.sgarrhh.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import py.sgarrhh.models.Rubro;
+
+
+public interface RubroRepository extends CrudRepository<Rubro, String>{
+	
+}
