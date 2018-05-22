c9bd6b7dd31799e54184e2bb2465a05cc4bf445f
denis <denis@denis-Aspire-F5-571T>
2018-05-22 11:59:09 -0400

    función

--- src/main/java/py/sgarrhh/controller/FuncionController.java
+++ src/main/java/py/sgarrhh/controller/FuncionController.java
@@ -1,12 +1,17 @@
 package py.sgarrhh.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import py.sgarrhh.models.Departamento;
 import py.sgarrhh.models.Funcion;
 import py.sgarrhh.repository.FuncionRepository;
 
@@ -22,9 +27,13 @@ public class FuncionController {
 	}
 
 	@RequestMapping(value="/registrarFuncion", method=RequestMethod.POST)
-	public String form(Funcion funcion) {
-	
+	public String form(@Valid Funcion funcion, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()){
+			attributes.addFlashAttribute("mensaje", "Verifique los campos!");
+			return "redirect:/f{id}";
+		}
 		fr.save(funcion);
+		attributes.addFlashAttribute("mensaje", "Registro guardado!");
 		
 		return "redirect:/registrarFuncion";
 	}
@@ -48,25 +57,23 @@ public class FuncionController {
 	}
 	
 	@RequestMapping(value="/f{id}", method=RequestMethod.POST)
-	private String detalleFuncionPost(Funcion funcion) {
+	private String detalleFuncionPost(@PathVariable("id") long id,@Valid Funcion funcion, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()){
+			attributes.addFlashAttribute("mensaje", "Verifique los campos!");
+			return "redirect:/f{id}";
+		}
 		fr.save(funcion);
-		
+		attributes.addFlashAttribute("mensaje", "Registro guardado!");
 		return "redirect:/listaFunciones";
 	}
 	
 	@RequestMapping("/eliminarFuncion")
-	private String eliminarFuncion(long id){
+	private String eliminarFuncion(long id , RedirectAttributes attributes){
 		Funcion funcion = fr.findById(id);
 		fr.delete(funcion);
+		attributes.addFlashAttribute("mensaje", "Eliminado con exito");
 		return "redirect:/listaFunciones";
 	}
 	
-	/*@RequestMapping(value="/{funciones}", method=RequestMethod.GET)
-	public ModelAndView selectFunciones() {
-		ModelAndView mv= new ModelAndView("cargo/formCargo");
-		Iterable <Funcion> funciones= fr.findAll();
-		mv.addObject("funciones",funciones);
-		return mv;
-	}
-	*/
+	
 }
