fc9aea2d2297b64c5bee5e48a696b52c60e7f2de
dsamarilla <denissaul87@gmail.com>
2018-08-30 17:37:55 -0400

    modif

--- src/main/java/py/sgarrhh/models/Concepto.java
+++ src/main/java/py/sgarrhh/models/Concepto.java
@@ -1,12 +1,10 @@
 package py.sgarrhh.models;
 
 import java.io.Serializable;
-import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
--- src/main/java/py/sgarrhh/models/Funcion.java
+++ src/main/java/py/sgarrhh/models/Funcion.java
@@ -9,8 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 
 
